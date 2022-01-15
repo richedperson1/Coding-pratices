7e5e33cadce8d8564e008057302c859753e51c3f
Rutvik Jaiswal <62417643+richedperson1@users.noreply.github.com>
2022-01-15 17:42:24 +0530

    Add files via upload

---
+++ DSA/Love_babbar/pratices/checking_palimdrom.py
@@ -0,0 +1,25 @@
+import re
+from tabnanny import check
+patten = '[^a-zA-Z0-9]'
+
+
+def checkPalindrome(s):
+    # Write your code here
+    # Return a boolean
+    n = len(s)
+    j = n-1
+    i = 0
+    while i<n and j >=0:
+        while j>=0 and (j == ' ' or bool(re.search(patten, s[j]))):
+            j -= 1
+
+        while i<n and( i == ' ' or bool(re.search(patten, s[i]))):
+            i+=1
+        if s[i].lower() != s[j].lower():
+            return 'No'
+        j -= 1
+        i+=1
+    return 'Yes'
+
+
+print(checkPalindrome('A1b22Ba'))

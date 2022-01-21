3d0ce76516ac44f01e965cebfc14d4a39432b2a3
Rutvik Jaiswal <62417643+richedperson1@users.noreply.github.com>
2022-01-21 15:39:37 +0530

    Add files via upload

--- DSA/Love_babbar/pratices/problem solving/search 2d matrix.py
+++ DSA/Love_babbar/pratices/problem solving/search 2d matrix.py
@@ -1,6 +1,6 @@
-arr = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+arr = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
 
-ele = 5
+ele = 0
 
 row = len(arr)
 col = len(arr[0])
@@ -14,13 +14,15 @@ def binary(arr,low,high,ele):
             high = mid-1
         else:
             low = mid+1
-
     return False
 
 ans = -1
 for i in range(row):
     if arr[i][0]<=ele and arr[i][-1]>=ele:
         ans = binary(arr[i],0,len(arr[i])-1,ele)
-        break
+        if ans:
+            print(True)
+            # exit
+            exit()
 
-print(ans)
+print(False)

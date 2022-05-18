f55779578923cc204bf5c268c8788c878090489b
Rutvik Jaiswal <62417643+richedperson1@users.noreply.github.com>
2022-05-18 16:59:40 +0530

    Update reverse_array.py

--- DSA/recurssion/reverse_array.py
+++ DSA/recurssion/reverse_array.py
@@ -29,4 +29,40 @@ def reverse_copy(arr, i, j):
 
 
 print(reverse_copy(array, 0, len(array)-1))
-print("The referance count of array is : ", sys.getrefcount(array))
+
+# ------------------------------------------------------------------------------------------------ #
+# Reverse array by using single pointer
+print("="*50)
+array = [2, 1, 3, 4, 5, 6, 7, 8]
+arr = copy.copy(array)
+
+
+def reverse_copy_using_1_var(arr, i):
+    if i < len(arr)-1-i:
+        arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
+        reverse_copy_using_1_var(arr, i+1)
+    return arr
+
+
+print(reverse_copy_using_1_var(array, 0))
+print(arr)
+print("="*50)
+print("The referance count of array is : ", sys.getrefcount('a'))
+
+
+# ---------------------------------------------------------------- #
+
+
+string = "mam"
+
+
+def check_palimdrom(string, i, j, flag):
+    if i < j:
+        flag = string[i] == string[j]
+        check_palimdrom(string, i+1, j-1, flag)
+        if flag == False:
+            return flag
+    return flag
+
+
+print(check_palimdrom(string, 0, len(string)-1, False))
